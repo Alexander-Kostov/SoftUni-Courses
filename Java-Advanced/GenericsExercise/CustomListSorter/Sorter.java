32b7282e69157700a4013870025aa732db2d54fc
Alexander <alex3747@abv.bg>
2022-07-15 14:32:51 +0300

    .

---
+++ Java-Advanced/GenericsExercise/CustomListSorter/Sorter.java
@@ -0,0 +1,14 @@
+package GenericsExercise.CustomListSorter;
+
+public class Sorter {
+    public static <T extends Comparable<T>> void sort(CustomList<T> list){
+        for (int i = 0; i < list.getSize(); i++) {
+            T current = list.get(i);
+            for (int j = i + 1; j < list.getSize(); j++) {
+                if (current.compareTo(list.get(j)) > 0){
+                    list.swap(i,j);
+                }
+            }
+        }
+    }
+}

b27e734388a38a03780937cce736dfb2690298fb
Alexander <alex3747@abv.bg>
2022-07-20 08:41:22 +0300

    .

---
+++ Java-Advanced/GenericsExercise/GenericSwapMethodIntegers/Main.java
@@ -0,0 +1,23 @@
+package GenericsExercise.GenericSwapMethodIntegers;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Box<Integer> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(scanner.nextLine());
+            box.add(num);
+        }
+
+        String [] data = scanner.nextLine().split(" ");
+        int firstIndex = Integer.parseInt(data[0]);
+        int secondIndex = Integer.parseInt(data[1]);
+        box.swap(firstIndex,secondIndex);
+        System.out.println(box);
+    }
+}

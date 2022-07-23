b71d25657d93abcb77dcbf959213f3529b1606d8
Alexander <alex3747@abv.bg>
2022-07-23 19:17:21 +0300

    .

---
+++ Java-Advanced/GenericsLab/ListUtils.java
@@ -0,0 +1,15 @@
+package GenericsLab;
+
+import java.util.List;
+
+public class ListUtils {
+
+    public static <T extends Comparable<T>> T getMin(List<T> elements) {
+        return elements.stream().min(T::compareTo).orElseThrow(() -> new IllegalArgumentException("Empty Collection"));
+    }
+
+
+    public static <T extends Comparable<T>> T getMax(List<T> elements) {
+        return elements.stream().max(T::compareTo).orElseThrow(() -> new IllegalArgumentException("Empty Collection"));
+    }
+}

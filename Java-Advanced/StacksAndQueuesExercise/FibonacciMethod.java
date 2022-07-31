527ebbc16f4dd873c8451c2961995b8701d16978
Alexander <alex3747@abv.bg>
2022-07-31 06:10:38 +0300

    .

---
+++ Java-Advanced/StacksAndQueuesExercise/FibonacciMethod.java
@@ -0,0 +1,32 @@
+package JavaAdvance.StacksAndQueuesExercise;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FibonacciMethod {
+    public static long[] memory;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        memory = new long[n + 1];
+
+        long result = fib(n);
+
+        System.out.println(result);
+    }
+
+    private static long fib(int n) {
+        if (n < 2) {
+            return 1;
+        }
+        if (memory[n] != 0) {
+            return memory[n];
+        }
+        memory[n] = fib(n - 1) + fib(n - 2);
+        return memory[n];
+    }
+}

f3e2f7d07b5181e7d4a33ae899ab16fa941203c0
Alexander <alex3747@abv.bg>
2022-08-06 19:53:56 +0300

    .

---
+++ Java-Advanced/MultidimensionalArraysLab/P03IntersectionOfTwoMatrices.java
@@ -0,0 +1,44 @@
+package JavaAdvance.MultidimensionalArraysLab;
+
+import java.util.Scanner;
+
+public class P03IntersectionOfTwoMatrices {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int rows = Integer.parseInt(scanner.nextLine());
+        int cols = Integer.parseInt(scanner.nextLine());
+
+
+        char [][] first = new char[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            first[row] = scanner.nextLine().replaceAll("\\s+","").toCharArray();
+        }
+        
+        char[][] second = new char[rows][cols];
+
+        for (int row = 0; row < second.length; row++) {
+            second[row] = scanner.nextLine().replaceAll("\\s+","").toCharArray();
+        }
+
+        char [][] output = new char[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                char firstElement = first[row][col];
+                char secondElement = second[row][col];
+
+                char fillChar = firstElement == secondElement ? firstElement : '*';
+                output[row][col] = fillChar;
+            }
+        }
+
+        for (char[] chars : output) {
+            for (char aChar : chars) {
+                System.out.print(aChar + " ");
+            }
+            System.out.println();
+        }
+    }
+}

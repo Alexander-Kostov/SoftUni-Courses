34467238143209ce352decacf2232c7330cad1eb
Alexander <alex3747@abv.bg>
2022-08-18 14:13:36 +0300

    .

---
+++ Java-Advanced/SetsAndMapsAdvancedLab/P09Larges3Numbers.java
@@ -0,0 +1,18 @@
+package JavaAdvance.SetsAndMapsAdvancedLab;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P09Larges3Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(Arrays.stream(scanner.nextLine().
+                split("\\s+")).
+                map(Integer::parseInt).
+                sorted(Comparator.reverseOrder()).limit(3).map(String::valueOf).
+                collect(Collectors.joining(" ")));
+    }
+}

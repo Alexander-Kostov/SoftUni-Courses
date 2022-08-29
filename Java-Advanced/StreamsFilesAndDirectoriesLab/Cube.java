621fb4e2684768d0aeefdff8dccfa504f63f943f
Alexander <alex3747@abv.bg>
2022-08-29 19:09:38 +0300

    .

---
+++ Java-Advanced/StreamsFilesAndDirectoriesLab/Cube.java
@@ -0,0 +1,10 @@
+package JavaAdvance.StreamsFilesAndDirectoriesLab;
+
+import java.io.Serializable;
+
+public class Cube implements Serializable {
+    String color;
+    double width;
+    double height;
+    double depth;
+}

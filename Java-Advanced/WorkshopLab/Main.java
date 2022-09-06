d0cc76944408850359ebd111350c69863d1ef816
Alexander <alex3747@abv.bg>
2022-09-06 21:20:12 +0300

    .

---
+++ Java-Advanced/WorkshopLab/Main.java
@@ -0,0 +1,14 @@
+package JavaAdvance.WorkshopLab;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        SmartArray<Integer> numbers = new SmartArray<>();
+        SmartArray<String> strings = new SmartArray<>();
+
+
+    }
+}

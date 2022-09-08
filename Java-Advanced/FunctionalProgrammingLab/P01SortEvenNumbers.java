fd3bc7c7b90d81030090dd93ece011bcc0d23f4b
Alexander <alex3747@abv.bg>
2022-09-08 19:49:15 +0300

    .

---
+++ Java-Advanced/FunctionalProgrammingLab/P01SortEvenNumbers.java
@@ -0,0 +1,22 @@
+package JavaAdvance.FunctionalProgrammingLab;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P01SortEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(", ")).map(Integer::parseInt)
+                .filter(e -> e % 2 == 0).collect(Collectors.toList());
+
+        String output = numbers.stream().map(String::valueOf).collect(Collectors.joining(", "));
+
+        System.out.println(output);
+
+        System.out.println(numbers.stream().sorted().map(String::valueOf).collect(Collectors.joining(", ")));
+
+    }
+}

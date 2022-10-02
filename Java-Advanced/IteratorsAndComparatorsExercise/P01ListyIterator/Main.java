0daef8958df7a7e2965446a84062541b45f7e36a
Alexander <alex3747@abv.bg>
2022-10-02 10:21:37 +0300

    .

---
+++ Java-Advanced/IteratorsAndComparatorsExercise/P01ListyIterator/Main.java
@@ -0,0 +1,47 @@
+package IteratorsComparatorsExerciseP01ListyIterator;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        ListyIterator listyIterator = null;
+        while (!input.equals("END")){
+            String [] parts = input.split(" ");
+            switch (parts[0]){
+                case "Create":
+                if (parts.length > 1){
+                    listyIterator = new ListyIterator(Arrays.copyOfRange(parts,1,parts.length));
+                }else {
+                    listyIterator = new ListyIterator();
+                }
+                    break;
+                case "Move":
+                    System.out.println(listyIterator.move());
+                    break;
+                case "HasNext":
+                    System.out.println(listyIterator.hasNext());
+                    break;
+                case "Print":
+                    try {
+                    listyIterator.print();
+                    } catch (IllegalStateException e){
+                        System.out.println("Invalid Operation!");
+                    }
+                    break;
+                case "PrintAll":
+                    for (String s : listyIterator) {
+                        System.out.print(s + " ");
+                    }
+                    System.out.println();
+                    break;
+            }
+
+            input = scanner.nextLine();
+        }
+    }
+}

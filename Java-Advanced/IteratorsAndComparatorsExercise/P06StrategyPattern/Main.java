fb6b7ebf5c082ada90d26621ba18ff74a2751153
Alexander <alex3747@abv.bg>
2022-10-07 22:56:40 +0300

    .

---
+++ Java-Advanced/IteratorsAndComparatorsExercise/P06StrategyPattern/Main.java
@@ -0,0 +1,33 @@
+package P06StrategyPattern;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        TreeSet<Person> nameSet = new TreeSet<>(new NameComparator());
+        TreeSet<Person> ageSet = new TreeSet<>(new AgeComparator());
+
+        for (int i = 0; i < n; i++) {
+            String [] data = scanner.nextLine().split(" ");
+            String name = data[0];
+            int age = Integer.parseInt(data[1]);
+
+            Person person = new Person(name,age);
+            ageSet.add(person);
+            nameSet.add(person);
+        }
+
+        for (Person person : nameSet) {
+            System.out.println(person);
+        }
+
+        for (Person person : ageSet) {
+            System.out.println(person);
+        }
+    }
+}

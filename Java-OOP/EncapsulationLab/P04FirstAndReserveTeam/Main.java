546444eae8e128a2138c6ae1b375c4da3d74f112
Alexander <alex3747@abv.bg>
2022-10-25 09:58:54 +0300

    .

---
+++ Java-OOP/EncapsulationLab/P04FirstAndReserveTeam/Main.java
@@ -0,0 +1,35 @@
+package EncapsulationLab.P04FirstAndReserveTeam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        List<Person> people = new ArrayList<>();
+
+        Team team = new Team("Black Eagles");
+
+
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split(" ");
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+            Person person = new Person(firstName, lastName, age, salary);
+            team.addPlayer(person);
+
+
+        }
+
+        System.out.printf("First team have %d players%n",team.getFirstTeam().size());
+        System.out.printf("Reserve team have %d players",team.getReserveTeam().size());
+
+    }
+}

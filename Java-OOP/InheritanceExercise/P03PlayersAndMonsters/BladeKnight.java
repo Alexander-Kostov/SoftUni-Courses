853e718376c27051baeedbfb8cb6f3c68190790d
Alexander <alex3747@abv.bg>
2022-11-01 09:19:35 +0200

    .

---
+++ Java-OOP/InheritanceExercise/P03PlayersAndMonsters/BladeKnight.java
@@ -0,0 +1,7 @@
+package InheritanceExercise.P03PlayersAndMonsters;
+
+public class BladeKnight extends DarkKnight{
+    public BladeKnight(String username, int level) {
+        super(username, level);
+    }
+}

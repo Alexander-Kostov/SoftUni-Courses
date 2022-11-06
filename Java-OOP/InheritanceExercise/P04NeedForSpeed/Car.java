f385144fc956a3ea49cce94eaf7b66464808af6a
Alexander <alex3747@abv.bg>
2022-11-06 10:27:10 +0200

    .

---
+++ Java-OOP/InheritanceExercise/P04NeedForSpeed/Car.java
@@ -0,0 +1,10 @@
+package InheritanceExercise.P04NeedForSpeed;
+
+public class Car extends Vehicle{
+    final static double DEFAULT_FUEL_CONSUMPTION = 3;
+
+    public Car(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}

b3180ece697e6d6760ef82fb6bcb17fd914b7f85
Alexander <alex3747@abv.bg>
2022-11-20 09:22:31 +0200

    .

---
+++ Java-OOP/InheritanceExercise/P06Animals/Kitten.java
@@ -0,0 +1,11 @@
+package InheritanceExercise.P06Animals;
+
+public class Kitten extends Cat {
+    public Kitten(String name, int age) {
+        super(name, age, "Female");
+    }
+
+    public String produceSound() {
+        return "Meow";
+    }
+}

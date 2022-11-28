8c1321ceb390987e12d09995ea5e04957294977c
Alexander <alex3747@abv.bg>
2022-11-28 12:33:48 +0200

    .

---
+++ Java-OOP/InheritanceLab/P04RandomArrayList/RandomArrayList.java
@@ -0,0 +1,12 @@
+package InheritanceLab.P04RandomArrayList;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList extends ArrayList<Object> {
+    public Object getRandomElement() {
+        Random random = new Random();
+        int index = random.nextInt(super.size());
+        return remove(index);
+    }
+}

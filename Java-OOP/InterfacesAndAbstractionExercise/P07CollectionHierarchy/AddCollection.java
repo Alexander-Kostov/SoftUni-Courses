fece9063a32bb5fd0dd74e8d55e79973f2fbcd5c
Alexander <alex3747@abv.bg>
2022-12-13 09:31:16 +0200

    .

---
+++ Java-OOP/InterfacesAndAbstractionExercise/P07CollectionHierarchy/AddCollection.java
@@ -0,0 +1,9 @@
+package InterfacesAndAbstractionExercise.P07CollectionHierarchy;
+
+public class AddCollection extends Collection implements Addable{
+    @Override
+    public int add(String element) {
+        items.add(element);
+        return items.indexOf(element);
+    }
+}

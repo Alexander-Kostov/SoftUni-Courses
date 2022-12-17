54acf2e006739477ec9ea8a9f82d5e60b5ee8090
Alexander <alex3747@abv.bg>
2022-12-17 09:46:34 +0200

    .

---
+++ Java-OOP/InterfacesAndAbstractionLab/P02CarShopExtend/Car.java
@@ -0,0 +1,12 @@
+package InterfacesAndAbstractionLab.P02CarShopExtend;
+
+public interface Car {
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String getCountryProduced();
+}

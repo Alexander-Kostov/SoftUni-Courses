490a1dd58e3b9aed7cf5e2a0f6845fb4311de375
Alexander <alex3747@abv.bg>
2022-12-22 11:44:37 +0200

    .

---
+++ Java-OOP/InterfacesAndAbstractionLab/P04SayHelloExtend/BasePerson.java
@@ -0,0 +1,17 @@
+package InterfacesAndAbstractionLab.P04SayHelloExtend;
+
+public abstract class BasePerson implements Person{
+
+    private String name;
+
+    protected BasePerson(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+
+}

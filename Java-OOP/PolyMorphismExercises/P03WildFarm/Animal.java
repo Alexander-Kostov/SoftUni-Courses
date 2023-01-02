43c46e61a96568bb5e2f8d406a66108f833365f4
Alexander <alex3747@abv.bg>
2023-01-02 11:06:35 +0200

    .

---
+++ Java-OOP/PolyMorphismExercises/P03WildFarm/Animal.java
@@ -0,0 +1,39 @@
+package PolyMorphismExercises.P03WildFarm;
+
+public abstract class Animal {
+
+    private String animalName;
+    private String animalType;
+    private Double animalWeight;
+    private Integer foodEaten;
+
+    public Animal(String animalName, String animalType, Double animalWeight) {
+        this.animalName = animalName;
+        this.animalType = animalType;
+        this.animalWeight = animalWeight;
+        this.foodEaten = 0;
+    }
+
+    public abstract void makeSound();
+    public abstract void eat(Food food);
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public Double getAnimalWeight() {
+        return animalWeight;
+    }
+
+    public Integer getFoodEaten() {
+        return foodEaten;
+    }
+
+    public void setFoodEaten(Integer foodEaten) {
+        this.foodEaten = foodEaten;
+    }
+}

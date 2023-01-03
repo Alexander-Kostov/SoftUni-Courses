982c1a98f90753cb5be505f5954cc80ec64db934
Alexander <alex3747@abv.bg>
2023-01-03 09:18:08 +0200

    .

---
+++ Java-OOP/PolyMorphismExercises/P03WildFarm/Felime.java
@@ -0,0 +1,8 @@
+package PolyMorphismExercises.P03WildFarm;
+
+public abstract class Felime extends Mammal{
+
+    public Felime(String animalName, String animalType, Double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight, livingRegion);
+    }
+}

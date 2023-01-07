aef739ff60883416fb67b901ec313fb8267abf32
Alexander <alex3747@abv.bg>
2023-01-07 17:23:00 +0200

    .

---
+++ Java-OOP/PolymorphismLab/P01MathOperation/MathOperation.java
@@ -0,0 +1,16 @@
+package PolymorphismLab.P01MathOperation;
+
+public class MathOperation {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return add(add(a, b), c);
+    }
+
+    public int add(int a, int b, int c, int d) {
+        return add(add(a, b, c), d);
+    }
+}

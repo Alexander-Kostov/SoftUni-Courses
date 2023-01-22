7ce43227af7c978c938cd4db529b91defc820e09
Alexander <alex3747@abv.bg>
2023-01-22 08:13:53 +0200

    .

---
+++ MySQL/DataDefinitionAndDatatypesLab/P02InsertDataTables.sql
@@ -0,0 +1,3 @@
+INSERT INTO `employees` (`first_name`, `last_name`) VALUES ('Test1', 'Test1');
+INSERT INTO `employees` (`first_name`, `last_name`) VALUES ('TEST2', 'TEST2');
+INSERT INTO `employees` (`first_name`, `last_name`) VALUES ('TEST3', 'TEST3');

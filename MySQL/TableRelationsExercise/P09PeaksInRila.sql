fe1f64036c2e5cb9767bbafc124aa847e5e15267
Alexander <alex3747@abv.bg>
2023-01-27 05:38:12 +0200

    .

---
+++ MySQL/TableRelationsExercise/P09PeaksInRila.sql
@@ -0,0 +1,6 @@
+SELECT m.`mountain_range`, p.`peak_name`, p.`elevation` 
+FROM `mountains` AS m
+INNER JOIN `peaks` AS p
+ON m.`id` = p.`mountain_id`
+WHERE `mountain_range` = 'Rila'
+ORDER BY `elevation` DESC;

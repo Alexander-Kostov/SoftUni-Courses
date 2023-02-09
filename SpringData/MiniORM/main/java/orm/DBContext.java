4524672f56a8ac61ec4de2aa947e6c9276e68dc7
Alexander <alex3747@abv.bg>
2023-02-09 07:37:46 +0200

    .

---
+++ SpringData/MiniORM/main/java/orm/DBContext.java
@@ -0,0 +1,20 @@
+package orm;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+
+public interface DBContext<E> {
+
+    boolean persist (E entity) throws SQLException, IllegalAccessException;
+
+    Iterable<E> find(Class<E> entityType) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException;
+    Iterable<E> find(Class<E> entityType, String where) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException;
+
+    E findFirst(Class<E> entityType) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException;
+    E findFirst(Class<E> entityType, String where) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException;
+
+    void doCreate(Class<E> entity) throws SQLException;
+
+    void doAlter(Class<E> entity) throws SQLException;
+
+}
